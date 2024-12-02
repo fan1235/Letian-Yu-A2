8d3790e6c16fae35f491b7e0aca67e4fec79c225
fan1296 <165129265+fan1235@users.noreply.github.com>
2024-12-02 21:00:37 +0800

    Modified the code of the first two parts

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,22 +1,7 @@
 public class AssignmentTwo {
 
     public static void main(String[] args) {
-        Employee operator = new Employee("John Doe", 30, "E001", "Ride Operator", 40);
-        Visitor visitor1 = new Visitor("Alice", 25, "V001", "Regular", 0);
-        Visitor visitor2 = new Visitor("Bob", 28, "V002", "VIP", 3);
 
-        Ride rollerCoaster = new Ride("Roller Coaster", operator, 5);
-
-        rollerCoaster.addVisitorToQueue(visitor1);
-        rollerCoaster.addVisitorToQueue(visitor2);
-
-        rollerCoaster.printQueue();
-
-        rollerCoaster.runOneCycle();
-
-        rollerCoaster.printRideHistory();
-
-        rollerCoaster.printQueue();
     }
 
     public void partThree() {
